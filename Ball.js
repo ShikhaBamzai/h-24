61bcd529667e3f9589fa669dee733908bf49d8a9
ShikhaBamzai <69149790+ShikhaBamzai@users.noreply.github.com>
2021-04-21 15:13:32 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,19 @@
+class Ball {
+    constructor(x,y,radius) {
+      var options = {
+          isStatic: false,
+          density: 1.2,
+          restitution:0.3,
+          friction:0.1
+      }
+      this.body = Bodies.circle(x,y,radius/2,options);
+      this.radius = radius
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("purple");
+      ellipse(pos.x, pos.y, this.radius, this.radius);
+    }
+  };
